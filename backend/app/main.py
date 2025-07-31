32e284dd9a6dbb5d040cbaffa7ed22f05a379bf2
BenjaminAGH <benjamin.guajardo2101@alumnos.ubiobio.cl>
2025-07-31 05:21:54 -0400

    coord

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,23 +4,19 @@ from app.modules.devices.controller import router as device_router
 
 from app.core.init_db import init_db
 from fastapi.middleware.cors import CORSMiddleware
-from app.core.cors import add_cors
-from app.core.config import settings
-
+from app.core.config import settings  # Importa bien
 
 app = FastAPI()
 
-init_db() 
+init_db()
 
 app.include_router(honeypot_router)
 app.include_router(device_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["settings.frontend_url"],
+    allow_origins=[settings.frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-add_cors(app)
